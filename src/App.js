cf4049a22972c6def35bdc2dfeb2fc1c6a6b82e7
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-06-13 19:30:58 +0800

    adding banner (incomplete)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
-import PostRegister from './PostRegister'
-import PostLogin from './PostLogin'
-import GetData from './GetData'
-import Error from './Error'
-import Navigation from './Navigation'
+import PostRegister from './component/PostRegister'
+import PostLogin from './component/PostLogin'
+import GetData from './component/GetData'
+import Error from './component/Error'
 
 class App extends Component {
   render(){
     return (
       <BrowserRouter>
-        <div>
-          <Navigation />
+        <div className>
+          <div className="banner">
+            <img src = "banner.png"></img>
+          </div>
           <Switch>
             <Route path="/" exact component={GetData}/>
             <Route path="/register" exact component={PostRegister} />     
